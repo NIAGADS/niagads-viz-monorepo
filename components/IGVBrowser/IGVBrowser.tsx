518f9439d0103fe1cd8e94bd72ffb6ecc564ec6a
fossilfriend <egreenfest@gmail.com>
2025-01-14 21:31:23 -0500

    lazy load igv

--- components/IGVBrowser/IGVBrowser.tsx
+++ components/IGVBrowser/IGVBrowser.tsx
@@ -6,41 +6,44 @@ import React, {
     useState,
     useEffect,
     useRef,
+    Suspense,
+    lazy
 } from "react";
-
-import igv from "igv/dist/igv.esm";
 
 import noop from "lodash.noop";
 import find from "lodash.find";
 
 import {
-    VariantPValueTrack,
+    //VariantPValueTrack,
     VariantServiceTrack as VariantTrack,
+    IGVBrowserTrack,
     trackPopover,
 } from "./tracks";
 
-import {
-    TrackBaseOptions,
-} from "./types/tracks";
+import { loadTracks, getLoadedTracks, removeTrackById } from "./decoders/utils";
 
-import {
-    loadTracks,
-    getLoadedTracks,
-    removeTrackById,
-} from "./decoders/utils";
-
-import { DEFAULT_FLANK, FEATURE_SEARCH_ENDPOINT } from "./config/_constants";
+import { DEFAULT_FLANK } from "./config/_constants";
 import { _genomes } from "./config/_igvGenomes";
+
+
+
+/* const igv = lazy(() =>
+    import("igv/dist/igv.esm").then((mod) => mod.igv)
+); */
+
+// import igv from "igv/dist/igv.esm";
 interface IGVBrowserProps {
     genome: string;
+    featureSearchURI: string;
+    tracks?: IGVBrowserTrack[];
     locus?: string;
     onTrackRemoved?: (track: string) => void;
     onBrowserLoad?: (Browser: any) => void;
-    tracks: TrackBaseOptions[];
 }
 
 const IGVBrowser: React.FC<IGVBrowserProps> = ({
     genome,
+    featureSearchURI,
     locus,
     onBrowserLoad,
     onTrackRemoved,
@@ -49,9 +52,12 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
     const [browserIsLoaded, setBrowserIsLoaded] = useState<boolean>(false);
     const [browser, setBrowser] = useState<any>(null);
     const [isClient, setIsClient] = useState(false);
+    const [igv, setIGV] = useState<any>(null);
+    const [pvalueTrackClass, setPvalueTrackClass] = useState<any>(null);
+
     const containerRef = useRef(null);
 
-    const memoOptions: any = useMemo(() => {
+    const opts: any = useMemo(() => {
         const referenceTrackConfig: any = find(_genomes, { id: genome });
         return {
             locus: locus || "APOE",
@@ -59,16 +65,9 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
             flanking: DEFAULT_FLANK,
             minimumBases: 40,
             search: {
-                url: `${FEATURE_SEARCH_ENDPOINT}$FEATURE$&flank=${DEFAULT_FLANK}`,
+                url: `${featureSearchURI}$FEATURE$&flank=${DEFAULT_FLANK}`,
             },
-            reference: {
-                id: genome,
-                name: referenceTrackConfig.name,
-                fastaURL: referenceTrackConfig.fastaURL,
-                indexURL: referenceTrackConfig.indexURL,
-                cytobandURL: referenceTrackConfig.cytobandURL,
-                tracks: referenceTrackConfig.tracks,
-            },
+            reference: referenceTrackConfig,
             loadDefaultGenomes: false,
             genomeList: _genomes,
         };
@@ -80,7 +79,7 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
 
     useEffect(() => {
         // setting initial session due to component load/reload
-        if (browserIsLoaded && memoOptions && tracks) {
+        if (browserIsLoaded && opts && tracks) {
             const loadedTracks = getLoadedTracks(browser);
 
             // if any tracks are loaded, remove them
@@ -95,59 +94,76 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
         }
     }, [browserIsLoaded]);
 
+    async function loadTrackClass(trackClass: string) {
+        const { default: t} = await import("./tracks/" + trackClass);
+        return t
+    }
+
     useLayoutEffect(() => {
-        /* window.addEventListener("ERROR: Genome Browser - ", (event) => {
-            console.log(event);
-        }); */
-
         if (isClient && containerRef.current) {
-            const targetDiv = containerRef.current;
-            // const targetDiv = document.getElementById("genome-browser");
+            // lazy load of igv library to avoid `window is not defined` ReferenceError
+            async function loadIGV() {
+                const { default: mod } = await import("igv/dist/igv.esm");
+                setIGV(mod);
+            }
+            async function loadTrackClasses() {
+                const t = await loadTrackClass('VariantPValueTrack')
+                setPvalueTrackClass(t);
+            }
 
-            if (memoOptions != null) {
-                igv.registerTrackClass("gwas_service", VariantPValueTrack);
-                igv.registerTrackClass("qtl", VariantPValueTrack);
-                igv.registerTrackClass("variant_service", VariantTrack);
+            if (!igv) {
+                loadIGV();
+                loadTrackClasses();
+            } else {
+                const targetDiv = containerRef.current;
 
-                igv.createBrowser(targetDiv, memoOptions).then(function (
-                    browser: any
-                ) {
-                    // custom track popovers
-                    browser.on("trackclick", trackPopover);
+                if (opts != null) {
+                    igv.registerTrackClass("gwas_service", pvalueTrackClass);
+                    // igv.registerTrackClass("qtl", VariantPValueTrack);
+                    igv.registerTrackClass("variant_service", VariantTrack);
 
-                    // perform action in encapsulating component if track is removed
-                    browser.on("trackremoved", function (track: any) {
-                        if (onTrackRemoved) {
-                            onTrackRemoved(track.config.id);
+                    igv.createBrowser(targetDiv, opts).then(function (
+                        browser: any
+                    ) {
+                        // custom track popovers
+                        browser.on("trackclick", trackPopover);
+
+                        // perform action in encapsulating component if track is removed
+                        browser.on("trackremoved", function (track: any) {
+                            if (onTrackRemoved) {
+                                onTrackRemoved(track.config.id);
+                            }
+                        });
+
+                        // add browser to state
+                        setBrowser(browser);
+                        setBrowserIsLoaded(true);
+
+                        // callback to parent component, if exist
+                        if (onBrowserLoad) {
+                            onBrowserLoad(browser);
+                        } else {
+                            noop();
                         }
                     });
-
-                    // add browser to state
-                    setBrowser(browser);
-                    setBrowserIsLoaded(true);
-
-                    // callback to parent component, if exist
-                    if (onBrowserLoad) {
-                        onBrowserLoad(browser);
-                    } else {
-                        noop();
-                    }
-                });
+                }
             }
         }
-    }, [isClient]);
+    }, [isClient, igv, pvalueTrackClass]);
 
-    if (!isClient) {
+    if (!isClient && !igv) {
         return <div>Loading...</div>;
     }
 
     return (
         <>
-            <span
-                ref={containerRef}
-                style={{ width: "100%" }}
-                id="genome-browser"
-            />
+            <Suspense fallback={<div>Loading...</div>}>
+                <span
+                    ref={containerRef}
+                    style={{ width: "100%" }}
+                    id="genome-browser"
+                />
+            </Suspense>
         </>
     );
 };
