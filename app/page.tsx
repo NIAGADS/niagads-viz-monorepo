b7584c8439c1c647bdc5910ed4cbdaa4d493666f
fossilfriend <egreenfest@gmail.com>
2025-01-21 11:12:46 -0500

    poc dynamic load

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import dynamic from 'next/dynamic'
-import { MemoIGVBrowser as GenomeBrowser } from "@/components/IGVBrowser/IGVBrowser";
+'use client'
+import dynamic from "next/dynamic";
+// import { MemoIGVBrowser as GenomeBrowser } from "@/components/IGVBrowser/IGVBrowser";
 
+const GenomeBrowser = dynamic(
+    () =>
+        import("@/components/IGVBrowser/IGVBrowser").then(
+            (mod) => mod.MemoIGVBrowser
+        ),
+    {
+        ssr: false,
+    }
+);
 
 export default function Home() {
-    return <GenomeBrowser featureSearchURI="/service/track/feature?id=" genome="hg38" tracks={[]}/>
+    return (
+        <GenomeBrowser
+            featureSearchURI="/service/track/feature?id="
+            genome="hg38"
+            tracks={[]}
+        />
+    );
 }
