2c5936913f8014cfc1152a09d192996c24ec3ced
fossilfriend <egreenfest@gmail.com>
2025-03-24 11:03:10 -0400

    resolve tailwind conflict in Table pacakge

--- packages/Table/src/index.ts
+++ packages/Table/src/index.ts
@@ -1,4 +1,8 @@
-import { Table } from "./Table";
+import "./styles/global.css";
 
+import Table from "./Table";
+export type { TableProps } from "./Table";
 export default Table;
+
 export * from "./TextRenderers"
+
