89f4e2bcdc2afd20b683b1b6bf36c5f1ae68a83c
fossilfriend <egreenfest@gmail.com>
2025-03-27 08:27:47 -0400

    common UI props (styling and ARIA)

---
+++ packages/UI/src/uiProps.ts
@@ -0,0 +1,13 @@
+
+export interface StylingProps {
+    colorVariant?: "primary" | "secondary" | "accent" | "default" | "transparent";
+    size?: "sm" | "md" | "lg";
+    className?: string;
+}
+
+export interface AriaProps {
+    role?: string;
+    ariaLabel?: string;
+    ariaLabeledBy?: string;
+    ariaDescribedBy?: string;
+}
