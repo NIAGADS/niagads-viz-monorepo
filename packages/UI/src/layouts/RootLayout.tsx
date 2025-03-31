e82e3d24abb44d05d5a1285bbe9f0472f933d09f
fossilfriend <egreenfest@gmail.com>
2025-03-31 02:55:55 -0400

    remove html tag

--- packages/UI/src/layouts/RootLayout.tsx
+++ packages/UI/src/layouts/RootLayout.tsx
@@ -6,19 +6,15 @@ import { Navigation } from "../Navigation";
 /**
  * RootLayout provides basic navigation and a container for the <main> section
  */
-export function RootLayout({ children, navConfig, fullWidth=false }: RootLayoutProps) {
+export function RootLayout({ children, navConfig, fullWidth = false }: RootLayoutProps) {
     return (
-        <html lang="en">
-            <body>
-                <div className="min-h-full">
-                    <header className="bg-white shadow-sm">
-                        <Navigation {...navConfig} />
-                    </header>
-                    <main className="mt-[60px] p-2">
-                        <div className={`ui-container ${fullWidth ? "full" : ""}`}>{children}</div>
-                    </main>
-                </div>
-            </body>
-        </html>
+        <div className="min-h-full">
+            <header className="bg-white shadow-sm">
+                <Navigation {...navConfig} />
+            </header>
+            <main className="mt-[60px] p-2">
+                <div className={`ui-container ${fullWidth ? "full" : ""}`}>{children}</div>
+            </main>
+        </div>
     );
 }
