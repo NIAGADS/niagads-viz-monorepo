57bcb6fdf880e23471fc5fe45d646259e8ec3fb3
fossilfriend <egreenfest@gmail.com>
2025-05-16 12:47:38 -0400

    fix return val

--- packages/Common/src/routeHandlers.ts
+++ packages/Common/src/routeHandlers.ts
@@ -1,38 +1,45 @@
-import * as path from 'path'
+import * as path from "path";
 
-import { NextRequest } from 'next/server'
+import { NextRequest } from "next/server";
 
-export function userAgentIsBrowser(userAgent: string):boolean {
+export function userAgentIsBrowser(userAgent: string): boolean {
     // checks if the user-agent for the request was a web browser
-    const browsers = ['Mozilla', 'Safari', 'Edge', 'Chrome']
-    return userAgent !== null && browsers.some(value => userAgent.includes(value))
+    const browsers = ["Mozilla", "Safari", "Edge", "Chrome"];
+    return userAgent !== null && browsers.some((value) => userAgent.includes(value));
 }
 
 async function __fetch(requestUri: string, asText: boolean = false) {
-    const response = await(fetch(requestUri))
+    const response = await fetch(requestUri);
     if (!response.ok) {
         throw new Error(`Fetch from ${requestUri} failed with status ${response.status}: ${response.statusText}`);
     }
-    const data = asText ? await response.text() : await response.json()
-    return data
+    const data = asText ? await response.text() : await response.json();
+    return data;
 }
 
-export async function backendFetchFromRequest(request: NextRequest, apiBaseUrl: string | undefined = process.env.API_SERVICE_URL, asText=false) {
+export async function backendFetchFromRequest(
+    request: NextRequest,
+    apiBaseUrl: string | undefined = process.env.API_SERVICE_URL,
+    asText = false
+) {
     if (!apiBaseUrl) {
-        throw new Error("`apiBaseUrl` cannot be null.  Please specify explicitly or set API_SERVICE_URL in .env.local")
+        throw new Error("`apiBaseUrl` cannot be null.  Please specify explicitly or set API_SERVICE_URL in .env.local");
     }
-    const incomingRequestUrl = new URL(request.url)
-    const pathname = incomingRequestUrl.pathname
-    const queryParams = incomingRequestUrl.search
-    const requestUri:string = path.join(apiBaseUrl, pathname, queryParams)
-    return await(__fetch(requestUri), asText)
+    const incomingRequestUrl = new URL(request.url);
+    const pathname = incomingRequestUrl.pathname;
+    const queryParams = incomingRequestUrl.search;
+    const requestUri: string = path.join(apiBaseUrl, pathname, queryParams);
+    return await __fetch(requestUri, asText);
 }
 
-export async function backendFetch(pathname: string, apiBaseUrl: string | undefined = process.env.API_SERVICE_URL, relative:boolean=false) {
+export async function backendFetch(
+    pathname: string,
+    apiBaseUrl: string | undefined = process.env.API_SERVICE_URL,
+    relative: boolean = false
+) {
     if (!apiBaseUrl) {
-        throw new Error("`apiBaseUrl` cannot be null.  Please specify explicitly or set API_SERVICE_URL in .env.local")
+        throw new Error("`apiBaseUrl` cannot be null.  Please specify explicitly or set API_SERVICE_URL in .env.local");
     }
-    const requestUri:string = path.join(apiBaseUrl, pathname)
-    return await(__fetch(requestUri))
+    const requestUri: string = path.join(apiBaseUrl, pathname);
+    return await __fetch(requestUri);
 }
-
