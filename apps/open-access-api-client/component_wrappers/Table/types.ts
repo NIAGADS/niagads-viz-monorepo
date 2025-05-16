af9cbc8095d97cda893fd9dba3f791bcf9e17dfc
fossilfriend <egreenfest@gmail.com>
2025-05-16 10:33:20 -0400

    remove all components etc that are part of the monorepo

--- apps/open-access-api-client/component_wrappers/Table/types.ts
+++
@@ -1,7 +0,0 @@
-import { TableProps } from "@/components/Table/Table";
-
-export interface TableWrapperProps {
-    table: TableProps
-    endpoint: string,
-    parameters: any, 
-}
--- apps/open-access-api-client/components/IGVBrowser/types/annotations.tsx
+++
@@ -1,9 +0,0 @@
-export interface VariantConsequence {
-    conseq: string;
-    impact: string;
-    is_coding: boolean;
-    codon_change: string;
-    amino_acid_change: string;
-    impacted_gene: string;
-    impacted_gene_symbol: string;
-}
