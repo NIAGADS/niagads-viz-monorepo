912e0e8102db94eeb45b89bfce8d1c3966d799ae
fossilfriend <egreenfest@gmail.com>
2025-05-16 12:12:31 -0400

    updated route template

--- apps/open-access-api-client/templates/route.ts
+++ apps/open-access-api-client/templates/route.ts
@@ -1,8 +1,8 @@
 // TEMPLATE route
-import { NextRequest } from 'next/server'
-import { get_with_redirect } from "@/utils/requestHandlers"
+import { NextRequest } from "next/server";
+import { backendFetchResponseHandler } from "@/utils/backend"
 
 export async function GET(request: NextRequest) {
-    return get_with_redirect(request);
+    return backendFetchResponseHandler(request);
 } 
 
