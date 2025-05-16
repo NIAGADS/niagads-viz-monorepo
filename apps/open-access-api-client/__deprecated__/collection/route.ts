7721686b09f703278f124b7e23ecb377a7ae6d14
fossilfriend <egreenfest@gmail.com>
2025-05-16 12:45:41 -0400

    move deprecated folder to app root

--- apps/open-access-api-client/__deprecated__/collection/route.ts
+++ apps/open-access-api-client/__deprecated__/collection/route.ts
@@ -1,7 +1,6 @@
 // TEMPLATE route
-import { backendFetchResponseHandler } from "@/utils/backend"
+import { backendFetchResponseHandler } from "@/utils/routeHandlers";
 
 export async function GET(request: NextRequest) {
     return backendFetchResponseHandler(request);
-} 
-
+}
