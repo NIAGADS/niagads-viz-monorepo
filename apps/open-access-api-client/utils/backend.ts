6b2bf8ebc1bce4fddf7464353ebd9e9b55f5c7aa
fossilfriend <egreenfest@gmail.com>
2025-05-16 10:41:51 -0400

    pass api url as a parameter

--- apps/open-access-api-client/utils/backend.ts
+++ apps/open-access-api-client/utils/backend.ts
@@ -1,27 +1,28 @@
 import { NextRequest } from 'next/server'
+import path from 'path'
 
-export async function backendFetch(request: NextRequest) {
+export async function backendFetch(apiServiceUrl: string, request: NextRequest) {
     const incomingRequestUrl = new URL(request.url)
     const path = incomingRequestUrl.pathname
     const queryParams = incomingRequestUrl.search
-    const requestUri:string = process.env.API_SERVICE_URL! + path + queryParams
+    const requestUri:string = apiServiceUrl! + path + queryParams
     const response = await(fetch(requestUri))
     const data = await response.json()
     return data
 }
 
-export async function backendFetchFromPath(path: string, relative:boolean=false) {
-    const requestUri:string = process.env.API_SERVER_URL! + path
+export async function backendFetchFromPath(apiServiceUrl: string, path: string, relative:boolean=false) {
+    const requestUri:string = apiServiceUrl + path
     const response = await(fetch(requestUri))
     const data = await response.json()
     return data
 }
 
-export async function backendFetchText(request: NextRequest) {
+export async function backendFetchText(apiServiceUrl: string, request: NextRequest) {
     const incomingRequestUrl = new URL(request.url)
     const path = incomingRequestUrl.pathname
     const queryParams = incomingRequestUrl.search
-    const requestUri:string = process.env.API_SERVICE_URL! + path + queryParams
+    const requestUri:string = apiServiceUrl + path + queryParams
     const response = await(fetch(requestUri))
     const data = await response.text()
     return data
