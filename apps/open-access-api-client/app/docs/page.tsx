642e40579672d1bffd5892f8a44f5ca1bdd37fd2
fossilfriend <egreenfest@gmail.com>
2025-05-21 08:02:26 -0700

    docs - in progress

--- apps/open-access-api-client/app/docs/page.tsx
+++ apps/open-access-api-client/app/docs/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { RedocStandalone } from "redoc";
 
+import { RedocStandalone } from "redoc";
+//https://redocly.com/docs/redoc/deployment/react
+//<RedocStandalone spec={/* spec as an object */} 
 export default function Page() {
     return (
         <main>
---
+++ apps/open-access-api-client/generate_docs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+if [ -f .env.local ]; then
+    export $(grep -v '^#' .env.local | xargs)
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+# Check if API_PUBLIC_URL variable is set
+if [ -z "$API_PUBLIC_URL" ]; then
+    echo "API_PUBLIC_URL variable not set in .env file!"
+    exit 1
+fi
+
+# Check if API_MAJOR_VERSION variable is set
+if [ -z "$API_MAJOR_VERSION" ]; then
+    echo "API_MAJOR_VERSION variable not set in .env file!"
+    exit 1
+fi
+
+# Define URLs using HOST_NAME from .env
+ROOT="${API_PUBLIC_URL}/v${API_MAJOR_VERSION}/openapi.yaml"
+FILER="${API_PUBLIC_URL}/filer/v${API_MAJOR_VERSION}/openapi.yaml"
+GENOMICS="${API_PUBLIC_URL}/genomics/v${API_MAJOR_VERSION}/openapi.yaml"
+
+# Download the OpenAPI YAML files
+wget -O docs/root.yaml "$ROOT"
+wget -O docs/filer.yaml "$FILER"
+wget -O docs/genomics.yaml "$GENOMICS"
+
+# Check if redocly is installed, install if not
+if ! command -v redocly &> /dev/null; then
+    npm install -g @redocly/cli
+fi
+
+
+# Merge the three files using Redocly CLI
+redocly join docs/root.yaml docs/genomics.yaml docs/filer.yaml \
+  --output docs/openapi.yaml 
+  --prefix-tags-with-filenames
+
+
+echo "Bundled OpenAPI file created as openapi.yaml"
