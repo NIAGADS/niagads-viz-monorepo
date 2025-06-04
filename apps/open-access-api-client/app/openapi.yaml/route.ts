8286efcd54afa10bca581a4801b70d34f95a51d2
fossilfriend <egreenfest@gmail.com>
2025-06-04 13:22:28 -0400

    dynamic routes

--- apps/open-access-api-client/app/openapi.yaml/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-import { backendFetchResponseHandler } from "@/utils/routeHandlers";
-
-export async function GET(request: NextRequest) {
-    return await backendFetchResponseHandler(request, true, {
-        "Content-Type": "text/yaml",
-    });
-}
