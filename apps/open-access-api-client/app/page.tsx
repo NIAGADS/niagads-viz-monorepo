678f73238518fdb13dee333924ab52969bd7931c
fossilfriend <egreenfest@gmail.com>
2025-06-05 15:05:10 -0400

    add versioning

--- apps/open-access-api-client/app/page.tsx
+++ apps/open-access-api-client/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { getCurrentVersion } from "@/utils/versioning";
 import { Alert, Button } from "@niagads/ui";
 
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const currentVersion = getCurrentVersion();
     const router = useRouter();
-
     const handleReadTheDocsClick = (e: any) => {
         e.preventDefault();
         router.push("/docs");
@@ -15,18 +16,8 @@ export default function Home() {
         <main>
             <div className="w-full">
                 <div className="mt-2 flex flex-col justify-center items-center">
-                    <Alert variant="info" message="THE NIAGADS Open Access API is being updated!">
-                        <div>
-                            <p>
-                                During this time some endpoints may fail and <strong>VIEWS</strong> (e.g., tables, IGV
-                                browser) will be unavailable, and requests to generate views against the API will only
-                                return JSON responses.
-                            </p>
-                            <p>Our apologies for the inconvenience.</p>
-                        </div>
-                    </Alert>
-                    <div className="text-secondary text-6xl">NIAGADS Open Access API</div>
-                    <div className="text-white text-lg mt-8">version 0.9.0a</div>
+                    <div className="text-6xl">NIAGADS Open Access API</div>
+                    <div className="text-lg mt-8">version {currentVersion}</div>
                     <div className="mt-8">
                         <Button size="lg" variant="default" onClick={handleReadTheDocsClick}>
                             Read the Docs
@@ -67,60 +58,3 @@ export default function Home() {
         </main>
     );
 }
-
-/*   <div className="pt-8 pb-8 flex flex-col justify-center items-center">
-                <div>NIAGADS Resources with API Access</div>
-                <div>
-                    The NIAGADS Open Access API will ultimately provide access
-                    to all public NIAGADS annotation resources. These incude the
-                    following:{" "}
-                </div>
-                <div>
-                    <div>genomicsDB</div>
-                    <div>
-                        <div>The NIAGADS Alzheimer's Genomics Database</div>
-                        <div>
-                            The NIAGADS Alzheimer's Genomics Database is
-                            disease-centric, interactive knowledgebase for
-                            Alzheimer's disease (AD) genetics. It provides a
-                            platform for data sharing, discovery, and analysis
-                            by making available AD-relevant GWAS summary
-                            statistics datasets from the NIAGADS repository for
-                            browsing and mining. Also available are detailed
-                            reports that compile genetic evidence for AD-risk
-                            with functional annotations in the context of gene
-                            and variants.
-                        </div>
-                        <div>
-                            <a href="https://www.niagads.org/genomics">
-                                https://www.niagads.org/genomics
-                            </a>
-                        </div>
-                        <div>
-                            <ul>
-                                <li>
-                                    Find the top risk-associated variants in
-                                    AD-relevant GWAS summary statistics
-                                    datasets.
-                                </li>
-                                <li>
-                                    Get AD-relevant genetic and functional
-                                    annotations in the context of a gene or
-                                    variant of interest.
-                                </li>
-                                <li>
-                                    Get Alzheimer's Disease Sequencing Project (
-                                    <a href="https://adsp.niagads.org">ADSP</a>)
-                                    variant QC results and annotations.
-                                </li>
-                                <li>
-                                    Fetch annotations for a single gene or
-                                    variant or do efficient bulk lookups
-                                    (feature lists or within a genomic region).
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            */
