e0bf7fcd41fcef9c0bd51415ddaa09eba7661726
fossilfriend <egreenfest@gmail.com>
2025-06-06 13:57:00 -0400

    test yaml

--- apps/open-access-api-client/app/docs/page.tsx
+++ apps/open-access-api-client/app/docs/page.tsx
@@ -2,14 +2,15 @@
 import React, { Suspense } from "react";
 import dynamic from "next/dynamic";
 import { Skeleton } from "@niagads/ui";
-
+// test yaml
+// https://raw.githubusercontent.com/OpenAPITools/openapi-generator/refs/heads/master/modules/openapi-generator/src/test/resources/3_0/petstore.yaml
 //FIXME: hardcoded version
 const RapiDoc = dynamic(() => import("@/component_wrappers/RapiDocWrapper"), { ssr: false });
 export default function Page() {
     return (
         <main>
             <Suspense fallback={<Skeleton type="default" />}>
-                <RapiDoc specUrl="v0/openapi.yaml" />
+                <RapiDoc specUrl="/v0/openapi.yaml" />
             </Suspense>
         </main>
     );
