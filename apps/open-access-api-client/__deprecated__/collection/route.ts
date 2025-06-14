50ab01055bc21daf5549013a42a9dbadfe0c69c7
fossilfriend <egreenfest@gmail.com>
2025-06-14 00:35:50 -0400

    remove deprecated

--- apps/open-access-api-client/__deprecated__/collection/route.ts
+++
@@ -1,6 +0,0 @@
-// TEMPLATE route
-import { backendFetchResponseHandler } from "@/utils/routeHandlers";
-
-export async function GET(request: NextRequest) {
-    return backendFetchResponseHandler(request);
-}
--- apps/open-access-api-client/__deprecated__/genomics/collection/[collection_id]/route.ts
+++
@@ -1,7 +0,0 @@
-// TEMPLATE route
-import { NextRequest } from "next/server";
-import { get_with_redirect } from "@/utils/requestHandlers";
-
-export async function GET(request: NextRequest) {
-    return get_with_redirect(request);
-}
