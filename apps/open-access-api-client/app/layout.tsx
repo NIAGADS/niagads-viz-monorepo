e36b16ffd920becd12975e639ffe678146ffe70c
fossilfriend <egreenfest@gmail.com>
2025-06-14 17:18:06 -0400

    navigation / home / remove theme dependency

--- apps/open-access-api-client/app/layout.tsx
+++ apps/open-access-api-client/app/layout.tsx
@@ -1,62 +1,57 @@
-import type { Metadata } from "next";
-import { ThemeProvider } from "@/components/contexts/theme-provider";
-import { Navbar } from "@/components/navbar";
-import { Space_Mono, Space_Grotesk } from "next/font/google";
-import { Footer } from "@/components/footer";
 import "@/styles/globals.css";
 
+import { Space_Grotesk, Space_Mono } from "next/font/google";
+
+import { Footer } from "@/components/footer";
+import type { Metadata } from "next";
+import { Navbar } from "@/components/navbar";
+import { ThemeProvider } from "@/components/contexts/theme-provider";
+import favicon from "@niagads/common/assets/images/favicon.ico";
+
 const sansFont = Space_Grotesk({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-  display: "swap",
-  weight: "400",
+    subsets: ["latin"],
+    variable: "--font-geist-sans",
+    display: "swap",
+    weight: "400",
 });
 
 const monoFont = Space_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-  display: "swap",
-  weight: "400",
+    subsets: ["latin"],
+    variable: "--font-geist-mono",
+    display: "swap",
+    weight: "400",
 });
 
 export const metadata: Metadata = {
-  title: "AriaDocs - Template",
-  metadataBase: new URL("https://ariadocs.vercel.app/"),
-  description:
-    "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.",
+    title: "NIAGADS Open Access API",
+    description: "Documentation and visualization endpoints for the NIAGADS Open Access API",
+    icons: {
+        icon: favicon.src,
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
-        />
-      </head>
-      <body
-        className={`${sansFont.variable} ${monoFont.variable} font-regular antialiased tracking-wide`}
-        suppressHydrationWarning
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          <main className="sm:container mx-auto w-[90vw] h-auto scroll-smooth">
-            {children}
-          </main>
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head>
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+                />
+            </head>
+            <body
+                className={`${sansFont.variable} ${monoFont.variable} font-regular antialiased tracking-wide`}
+                suppressHydrationWarning
+            >
+                <Navbar />
+                <main className="sm:container mx-auto w-[90vw] h-auto scroll-smooth">{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
