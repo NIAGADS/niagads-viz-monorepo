4fc3880c569865e136babd9167bc6323ebfdc59a
fossilfriend <egreenfest@gmail.com>
2025-07-14 20:34:42 -0400

    updated types

--- apps/genomicsdb/lib/types.ts
+++ apps/genomicsdb/lib/types.ts
@@ -1,9 +1,21 @@
-export interface APIResponse {
-    data: Data[];
-    request: Request;
-    pagination: Pagination;
-    message: string;
-}
+// lib/types.ts - project type definitions
+
+// literal types
+
+export type PageSectionIcons =
+    | "home"
+    | "gantt"
+    | "barchart"
+    | "link"
+    | "network"
+    | "database"
+    | "info"
+    | "activity"
+    | "file"
+    | "frequency";
+export type RecordType = "gene" | "variant" | "span" | "track";
+
+// API Response
 
 export interface Data {}
 
@@ -22,4 +34,110 @@ export interface Pagination {
     total_num_pages: number;
     paged_num_records: number;
     total_num_records: number;
+}
+
+export interface APIResponse {
+    data: Data[];
+    request: Request;
+    pagination: Pagination;
+    message: string;
+}
+
+// Page Request Parameters
+
+export interface PageProps {
+    params: Promise<{
+        type: string;
+        id: string;
+    }>;
+    searchParams: Promise<{
+        [key: string]: string | string[] | undefined;
+    }>;
+}
+
+// Records
+
+export interface BaseRecord {
+    id: string;
+    type: RecordType;
+}
+
+export interface GenomicLocation {
+    start: number;
+    end: number | null;
+    length: number | null;
+    chr: string;
+    strand: "+" | "-";
+}
+
+interface GeneFeature extends BaseRecord {
+    type: "gene";
+    gene_symbo: string;
+}
+
+export interface GeneRecord extends GeneFeature {
+    type: "gene";
+    gene_type: string;
+    gene_name: string | null;
+    synonyms: string[] | null;
+    location: GenomicLocation;
+    cytogenic_location: string | null;
+}
+
+export interface PredictedConsequence {
+    consequence_terms: string[];
+    impact: "HIGH" | "MODERATE" | "LOW" | "MODIFIER";
+    is_coding: boolean | null;
+    impacted_gene: GeneFeature | null;
+    impacted_transcript: string | null;
+    codon_change: string | null;
+    amino_acid_cahnge: string | null;
+}
+
+export interface VariantRecord extends BaseRecord {
+    type: "variant";
+    variant_class: string;
+    id: string;
+    ref_snp_id: string;
+    location: GenomicLocation;
+    is_adsp_variant: boolean | null;
+    most_severe_consequence: PredictedConsequence | null;
+    ref: string;
+    alt: string;
+}
+
+export interface SpanRecord extends BaseRecord {
+    type: "span";
+    location: GenomicLocation;
+}
+
+export interface TrackRecord extends BaseRecord {
+    type: "track";
+    id: string;
+    name: string;
+    description: string;
+    genome_build: "GRCh37" | "GRCh38";
+    feature_type: string;
+    data_source: string;
+    url: string;
+}
+
+export type Record = GeneRecord | VariantRecord | SpanRecord | TrackRecord;
+export type GenomicFeatureRecord = GeneRecord | VariantRecord | SpanRecord;
+
+// record page structure; anchored sections
+
+interface AnchoredSectionBase {
+    id: string;
+    label: string;
+    description?: string | React.ReactNode;
+}
+
+export interface TableSection extends AnchoredSectionBase {
+    endpoint: string;
+}
+
+export interface AnchoredPageSection extends AnchoredSectionBase {
+    icon: PageSectionIcons;
+    tables?: TableSection[];
 }
