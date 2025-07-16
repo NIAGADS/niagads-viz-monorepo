54ca70b327ed2ed84baf35ef8a3ef732b0b44fa9
fossilfriend <egreenfest@gmail.com>
2025-07-16 08:48:31 -0400

    recharts - failing test

--- packages/Charts/src/AssociationSummaryChart.tsx
+++ packages/Charts/src/AssociationSummaryChart.tsx
@@ -1,12 +1,8 @@
-import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, Label, Legend, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 import React from "react";
 
 // dataKey: value pairs
-export interface SummaryData {
-    trait_category: string;
-    [key: string]: number | string;
-}
 
 // dataKey: legend label pairs
 export interface Series {
@@ -14,17 +10,17 @@ export interface Series {
 }
 
 export interface GeneticAssociationSummaryChartConfig {
-    data: SummaryData[];
+    data: any;
     series: Series;
 }
 
-export const AssociationSummaryBarChart = ({ config }: { config: GeneticAssociationSummaryChartConfig }) => {
+export const AssociationSummaryBarChart = ({ data, series }: GeneticAssociationSummaryChartConfig) => {
     return (
         <ResponsiveContainer width="100%" height="100%">
             <BarChart
                 width={500}
                 height={300}
-                data={config.data}
+                data={data}
                 margin={{
                     top: 20,
                     right: 30,
@@ -32,14 +28,42 @@ export const AssociationSummaryBarChart = ({ config }: { config: GeneticAssociat
                     bottom: 5,
                 }}
             >
-                <XAxis label="Trait Category" dataKey="trait_category" />
-                <YAxis />
-                <Tooltip />
+                <XAxis dataKey="trait_category">
+                    <Label value="Trait Category" position="bottom" />
+                </XAxis>
+                <YAxis>
+                    <Label value="No. Variants" />
+                </YAxis>
+
                 <Legend />
-                {Object.entries(config.series).map(([dataKey, label]) => {
-                    <Bar key={dataKey} name={label} dataKey={dataKey} stackId={dataKey.split("|")[0]} />;
-                })}
+
+                <Bar name={"AD"} dataKey="upstream" stackId="gwas" />
             </BarChart>
         </ResponsiveContainer>
     );
 };
+
+/*  {Object.entries(series).map(([dataKey, label]) =>
+                    dataKey === "total" ? null : <Bar name={label} dataKey={dataKey} stackId={dataKey.split("|")[0]} />
+                )}*/
+/*
+                     {
+[1]     trait_category: 'gwas|adrd',
+[1]     uptream: 307,
+[1]     downstream: 148,
+[1]     in_gene: 0,
+[1]     upstream: 182
+[1]   },
+[1]   {
+[1]     trait_category: 'gwas|ad_adrd_biomarkers',
+[1]     uptream: 131,
+[1]     downstream: 54,
+[1]     in_gene: 0
+[1]   },
+1]   {
+[1]     trait_category: 'curated|non-ad_adrd',
+[1]     uptream: 83,
+[1]     downstream: 61,
+[1]     in_gene: 0,
+[1]     upstream: 64
+[1]   },*/
