d51f5a5289fd2ab2c2625bff79e544583618093f
fossilfriend <egreenfest@gmail.com>
2025-07-18 08:17:07 -0400

    fix linting issues

--- packages/Table/postcss.config.js
+++ packages/Table/postcss.config.js
@@ -1,6 +1,5 @@
-
 module.exports = {
     plugins: {
-        "postcss-import": {} // Ensures nested @import rules are processed
+        "postcss-import": {}, // Ensures nested @import rules are processed
     },
 };
