9060c3b0ea48e96b1a42119c9284af7ccde597ec
fossilfriend <egreenfest@gmail.com>
2025-07-19 18:08:21 -0400

    basic text and badges

--- packages/Table/src/CellRenderers/BasicText.tsx
+++ packages/Table/src/CellRenderers/BasicText.tsx
@@ -1,10 +1,39 @@
-import React, { useState } from "react";
-
+import React, { ReactNode, useState } from "react";
+import { TextRenderer, buildElementStyle, renderNullValue } from "./TextRenderer";
 import { _deepCopy, _get, _hasOwnProperty, _isJSON, _isNA, _isNull } from "@niagads/common";
+import { renderTooltip, renderWithHelpIcon } from "@niagads/ui/client";
 
-import { TextRenderer, renderWithInfo, renderStyledText, renderNullValue, buildElementStyle } from "./TextRenderer";
+import { StylingProps } from "@niagads/ui";
+import styles from "../styles/cell.module.css";
 
 const DEFAULT_MAX_LENGTH = 100;
+
+interface StyledTextProps extends StylingProps {
+    value: string;
+}
+
+export const StyledText = ({ value, className = "", style = {} }: StyledTextProps) => {
+    return (
+        <span className={`${className}`} style={style}>
+            {value}
+        </span>
+    );
+};
+
+interface TextWithInfoProps {
+    text: ReactNode;
+    message: string;
+    anchorId: string;
+    asInfoLink?: boolean; // display as info link instead of as icon
+}
+
+export const TextWithInfo = ({ text, message, anchorId, asInfoLink }: TextWithInfoProps) => {
+    if (asInfoLink) {
+        return <>{renderTooltip(anchorId, text, message)}</>;
+    }
+
+    return renderWithHelpIcon(text, "info", message, anchorId);
+};
 
 export const TextList = <T,>({ props }: TextRenderer<T>) => {
     const items = _get("items", props);
@@ -23,6 +52,7 @@ export const TextList = <T,>({ props }: TextRenderer<T>) => {
 export const Text = <T,>({ props }: TextRenderer<T>) => {
     const value = _get("value", props);
 
+    // handle NAs
     if (_isNull(value)) {
         return renderNullValue(_get("nullValue", props));
     }
@@ -31,24 +61,31 @@ export const Text = <T,>({ props }: TextRenderer<T>) => {
         return renderNullValue();
     }
 
+    // handle large text
     const maxLength = _get("truncateTo", props, DEFAULT_MAX_LENGTH);
     if (value.length > maxLength) {
         return <LargeText props={props} />;
     }
 
-    const hasTooltip = _hasOwnProperty("tooltip", props);
-    const useInfoLink = _get("inlineTooltip", props, false); // tooltip rendered as info link instead of info icon
+    // styling
     const style = buildElementStyle(props);
+    const styledText = <StyledText value={value} style={style} />;
 
-    const textElement = renderStyledText(value, style, hasTooltip && useInfoLink ? "info-link" : "");
-    return hasTooltip
-        ? renderWithInfo(
-              textElement,
-              _get("tooltip", props),
-              `${_get("rowId", props)}-${_get("columnId", props)}`,
-              useInfoLink
-          )
-        : textElement;
+    // tooltips
+    if (_hasOwnProperty("tooltip", props)) {
+        const anchorId = `${_get("rowId", props)}-${_get("columnId", props)}`;
+        const asInfoLink = _get("inlineTooltip", props, false); // tooltip
+        return (
+            <TextWithInfo
+                text={styledText}
+                message={_get("tooltip", props)}
+                anchorId={anchorId}
+                asInfoLink={asInfoLink}
+            />
+        );
+    }
+
+    return styledText;
 };
 
 export const LargeText = <T,>({ props }: TextRenderer<T>) => {
@@ -78,14 +115,16 @@ export const LargeText = <T,>({ props }: TextRenderer<T>) => {
         return <Text props={newProps} />;
     }
 
+    // styling
     const style = buildElementStyle(props);
-    const textElement = renderStyledText(isExpanded ? value : truncatedValue, style, "");
+    const styledText = <StyledText value={isExpanded ? value : truncatedValue} style={style} />;
+
     const action = isExpanded ? "Show Less" : "Show More";
     return (
-        <div className="max-w-[300px] break-words">
-            {textElement}
+        <div className={styles.truncatedText}>
+            {styledText}
             {"   "}
-            <a className="text-xs ui-info-link" onClick={toggleIsExpanded}>
+            <a className={`text-xs ${styles.infoLink}`} onClick={toggleIsExpanded}>
                 {action}
             </a>
         </div>
