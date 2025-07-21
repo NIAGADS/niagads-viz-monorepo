ad8f0c86829af9ca406f683adde94d868385d5b2
fossilfriend <egreenfest@gmail.com>
2025-07-21 01:55:27 -0400

    pagination message

--- apps/genomicsdb/components/PaginationMessage.tsx
+++ apps/genomicsdb/components/PaginationMessage.tsx
@@ -3,8 +3,8 @@
 import { Alert, Button, InlineIcon } from "@niagads/ui";
 import React, { useId } from "react";
 
+import { CloudCog } from "lucide-react";
 import { Pagination } from "@/lib/types";
-import { Share2 } from "lucide-react";
 import { Tooltip } from "@niagads/ui/client";
 import styles from "./styles/pagination-message.module.css";
 
@@ -23,21 +23,34 @@ const PaginationMessage = ({ pagination, endpoint }: PaginationMessageProps) => 
     };
 
     return (
-        <Alert variant="info" message={"This search returned a large result"}>
-            <div className={styles.pagination}>
-                Only <b>{pagination.paged_num_records.toLocaleString()}</b> out of{" "}
-                <b>{pagination.total_num_records.toLocaleString()}</b> records are shown here.
-                <br />
-                You can retrieve the full result set directly from the NIAGADS Open Access API. See the{" "}
-                <a href={`${process.env.NEXT_PUBLIC_API_URL}/docs`} target="_blank">
-                    API documentation
-                </a>{" "}
-                for more information.
-                <Tooltip content="Copied to clipboard!" anchorId={tooltipId} openOnClick={true}>
-                    <Button onClick={handleCopy} title={`Get record permalink`}>
-                        <InlineIcon icon={<Share2 size={16} />}>Share</InlineIcon>
-                    </Button>
-                </Tooltip>
+        <Alert variant="info" message={"This query returned a large result"} className={styles.alert}>
+            <div className={styles.messageContainer}>
+                <div>
+                    <Tooltip content="Copied to clipboard!" anchorId={tooltipId} openOnClick={true}>
+                        <Button
+                            onClick={handleCopy}
+                            title={`Get record permalink`}
+                            color="primary"
+                            style={{ marginRight: "1rem" }}
+                        >
+                            <InlineIcon icon={<CloudCog size={16} />}>Fetch</InlineIcon>
+                        </Button>
+                    </Tooltip>
+                </div>
+                <div className={styles.message}>
+                    <span>
+                        Only <b>{pagination.paged_num_records.toLocaleString()}</b> out of{" "}
+                        <b>{pagination.total_num_records.toLocaleString()}</b> records are shown here.
+                    </span>
+                    <br />
+                    <span>
+                        You can retrieve the full result set directly from the NIAGADS Open Access API. See the{" "}
+                        <a href={`${process.env.NEXT_PUBLIC_API_URL}/docs`} target="_blank">
+                            API documentation
+                        </a>{" "}
+                        for more information.
+                    </span>
+                </div>
             </div>
         </Alert>
     );
