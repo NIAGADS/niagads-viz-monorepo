0f4bb619d3eb8563260ef222f3b522d987f0e2cd
fossilfriend <egreenfest@gmail.com>
2025-07-21 01:55:44 -0400

    add in tables

--- apps/genomicsdb/components/records/RecordTable.tsx
+++ apps/genomicsdb/components/records/RecordTable.tsx
@@ -1,17 +1,19 @@
-import { APITableResponse, CacheIdentifier, TableSection } from "@/lib/types";
+import { APITableResponse, CacheIdentifier, Pagination, TableSection } from "@/lib/types";
 import { Alert, LoadingSpinner } from "@niagads/ui";
 import { InlineError, NoData } from "../ErrorAlerts";
 
 import PaginationMessage from "../PaginationMessage";
+import Table from "@niagads/table";
 import { _fetch } from "@/lib/route-handlers";
 import { is_error_response } from "@/lib/utils";
 import useSWR from "swr";
 
 export interface RecordTableProps extends CacheIdentifier {
     tableDef: TableSection;
+    onTableLoad?: (pagination: Pagination) => void;
 }
 
-export default function RecordTable({ tableDef, ...cacheInfo }: RecordTableProps) {
+export default function RecordTable({ tableDef, onTableLoad, ...cacheInfo }: RecordTableProps) {
     const { data, error, isLoading } = useSWR(
         `/record/${cacheInfo.recordType}/${cacheInfo.recordId}/annotation/${tableDef.endpoint}`,
         (url: string) => fetch(url).then((res) => res.json())
@@ -25,6 +27,12 @@ export default function RecordTable({ tableDef, ...cacheInfo }: RecordTableProps
         return <InlineError message={data.detail} reload={false} />;
     }
 
+    // Call onTableLoad when data is loaded and valid to return the result size
+    // to the parent
+    if (onTableLoad && data) {
+        onTableLoad((data as APITableResponse).pagination);
+    }
+
     if ((data as APITableResponse).pagination.total_num_records === 0) {
         return <NoData />;
     }
@@ -36,10 +44,21 @@ export default function RecordTable({ tableDef, ...cacheInfo }: RecordTableProps
                     pagination={(data as APITableResponse).pagination}
                     endpoint={`/record/${cacheInfo.recordType}/${cacheInfo.recordId}${tableDef.endpoint}`}
                 />
+                <Table
+                    id={tableDef.id}
+                    columns={(data as APITableResponse).table.columns}
+                    data={(data as APITableResponse).table.data}
+                />
             </>
         );
 
-    return <Alert message="hope this works" />;
+    return (
+        <Table
+            id={tableDef.id}
+            columns={(data as APITableResponse).table.columns}
+            data={(data as APITableResponse).table.data}
+        />
+    );
 }
 
 //<Table config={tableDef} table={response.table} />
