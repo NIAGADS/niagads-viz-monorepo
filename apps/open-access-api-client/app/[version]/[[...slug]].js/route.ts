6746b8a186ed416a4ab768717cb4f584b888ff2d
fossilfriend <egreenfest@gmail.com>
2025-08-05 09:24:16 -0400

    remove open access api client from monorepo due to legacy dependencies incl tailwindcss and materialui

--- apps/open-access-api-client/app/[version]/[[...slug]].js/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-import { backendFetchResponseHandler } from "@/lib/route-handlers";
-
-export async function GET(request: NextRequest) {
-	return await backendFetchResponseHandler(request);
-}
