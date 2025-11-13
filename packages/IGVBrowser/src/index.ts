02d5a23286149bd3afd9dc1e76b503360df63f40
fossilfriend <egreenfest@gmail.com>
2025-11-12 21:54:26 -0500

    add exports

--- packages/IGVBrowser/src/index.ts
+++ packages/IGVBrowser/src/index.ts
@@ -1,9 +1,7 @@
 import IGVBrowser from "./IGVBrowser";
 import IGVBrowserWithSelector from "./IGVBrowserWithSelector";
-import TrackSelectorTable  from "./TrackSelectorTable";
+import TrackSelectorTable from "./TrackSelectorTable";
+import { VariantReferenceTrack } from "./config/_constants";
 
-export {
-    IGVBrowser,
-    IGVBrowserWithSelector,
-    TrackSelectorTable,
-}
+export { IGVBrowser, IGVBrowserWithSelector, TrackSelectorTable, VariantReferenceTrack };
+export type { IGVBrowserTrack } from "./types/data_models";
