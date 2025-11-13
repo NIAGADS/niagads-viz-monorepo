97348f85de3ae29550361cda702022aecce5d856
Sam Tate <samuel.tate@outlook.com>
2025-11-13 14:54:39 -0500

    introduce inline gene browser

---
+++ packages/Charts/src/InlineGeneBrowser.tsx
@@ -0,0 +1,11 @@
+
+interface InlineGeneBrowserProps {
+    flankLength: number;
+    geneLength: number;
+    startLocation: number;
+    endLocation: number;
+}
+
+const InlineGeneBrowser = ({}: InlineGeneBrowserProps) => {
+    return <></>;
+}
