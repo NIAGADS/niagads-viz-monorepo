5cd35abcc4f5a32c63dc477afa06c5f30ca3031c
fossilfriend <egreenfest@gmail.com>
2026-02-05 00:13:12 -0500

    export trackselectortable

--- packages/IGVBrowser/src/index.ts
+++ packages/IGVBrowser/src/index.ts
@@ -1,6 +1,7 @@
 import IGVBrowser from "./IGVBrowser";
-import IGVBrowserWithSelector from "./IGVBrowserWithSelector";
+import TrackSelectorTable from "./TrackSelectorTable";
 import { VariantReferenceTrack } from "./config/_constants";
-import { convertFILERMetadataToIGVTrack } from "./tracks/utils";
+
 export type { IGVBrowserTrack } from "./types/data_models";
-export { convertFILERMetadataToIGVTrack, IGVBrowser, IGVBrowserWithSelector, VariantReferenceTrack };
+export type { TableProps, RowSelectionState } from "./TrackSelectorTable";
+export { IGVBrowser, VariantReferenceTrack, TrackSelectorTable };
