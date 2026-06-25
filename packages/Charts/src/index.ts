544c2511efe6a3a61b64ecaa777767919bce261b
fossilfriend <egreenfest@gmail.com>
2026-06-25 09:52:53 -0400

    association summary stacked bar; moved data transformation into charts package (so can be used in API)

--- packages/Charts/src/index.ts
+++ packages/Charts/src/index.ts
@@ -1,8 +1,17 @@
 import Histogram, { RangeSelectHistogram, ThresholdSelectHistogram } from "./Histogram/Histogram";
 import PieChart, { PieChartDataRow } from "./PieChart/PieChart";
+import GeneticAssociationStackedBarChart, { GeneticAssociationCountRow } from "./StackedBarChart/GeneticAssociationStackedBarChart";
 import StackedBarChart, { StackedBarChartDataRow } from "./StackedBarChart/StackedBarChart";
 
 import BarChart from "./BarChart";
 
-export { BarChart, Histogram, RangeSelectHistogram, ThresholdSelectHistogram, PieChart, StackedBarChart };
-export type { PieChartDataRow, StackedBarChartDataRow };
+export {
+    BarChart,
+    Histogram,
+    RangeSelectHistogram,
+    ThresholdSelectHistogram,
+    PieChart,
+    StackedBarChart,
+    GeneticAssociationStackedBarChart,
+};
+export type { GeneticAssociationCountRow, PieChartDataRow, StackedBarChartDataRow };
