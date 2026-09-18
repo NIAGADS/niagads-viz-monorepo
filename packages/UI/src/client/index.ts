ca27348bff53f314779a535f684110b7e5aa8545
fossilfriend <egreenfest@gmail.com>
2026-09-18 14:59:28 -0400

    add index.ts files to simplify imports

--- packages/UI/src/client/index.ts
+++ packages/UI/src/client/index.ts
@@ -1,10 +1,10 @@
 import "../global.css";
 
-export * from "./ActionMenu/ActionMenu";
+export * from "./ActionMenu";
 export * from "./APISearch";
-export * from "./Autocomplete/Autocomplete";
-export * from "./RichSelect/RichSelect";
-export * from "./Slider/Slider";
-export * from "./Tabs/Tabs";
+export * from "./Autocomplete";
+export * from "./RichSelect";
+export * from "./Slider";
+export * from "./Tabs";
 export * from "./TooltipClient";
-export * from "./CollapsibleSection/CollapsibleSection";
+export * from "./CollapsibleSection";
