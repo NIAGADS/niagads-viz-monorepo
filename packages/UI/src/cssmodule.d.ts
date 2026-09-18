dc7df02f21240b0943cd51258517d7dcec4ef7dd
fossilfriend <egreenfest@gmail.com>
2026-09-18 14:40:58 -0400

    add ambient type for .css files

--- packages/UI/src/cssmodule.d.ts
+++ packages/UI/src/cssmodule.d.ts
@@ -4,3 +4,9 @@ declare module "*.module.css" {
     const classes: { [key: string]: string };
     export default classes;
 }
+
+// allows importing of bare css files (e.g., global.css)
+declare module "*.css" {
+    const content: string;
+    export default content;
+}
